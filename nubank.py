932dcbb382e9cfc5f08d7d76037d806f90431ec7
richard <richard.silva@neoway.com.br>
2015-08-10 11:46:24 -0300

    First commit

---
+++ nubank.py
@@ -0,0 +1,30 @@
+import pdfquery
+
+pdf = pdfquery.PDFQuery("/home/richard/Downloads/fatura.pdf")
+pdf.load()
+
+label = pdf.pq('LTTextLineHorizontal:contains("Valor")')
+x1 = float(label.attr('x0'))
+y1 = float(label.attr('y0'))
+
+nome = pdf.pq('LTTextLineHorizontal:in_bbox("%s, %s, %s, %s")' % (x1, y1, x1+300, y1+60)).text()
+print nome
+
+label =  pdf.pq('LTTextLineHorizontal:contains("Vencimento")')
+x1 = float(label.attr('x0'))
+y1 = float(label.attr('y0'))
+
+vencimento =  pdf.pq('LTTextLineHorizontal:in_bbox("%s, %s, %s, %s")' %  (x1, y1, x1+300, y1+20)).text()
+print vencimento
+
+count = 0
+
+while True:
+    x1 = 148.648
+    y1 = 524.118 - (35.9030*count)
+    count += 1
+    despesa = pdf.pq('LTTextLineHorizontal:in_bbox("%s, %s, %s, %s")' %  (x1, y1, x1+300, y1+20)).text()
+    valor = pdf.pq('LTTextLineHorizontal:in_bbox("%s, %s, %s, %s")' %  (x1+200, y1, x1+600, y1+20)).text()
+    if despesa == "" or valor == "":
+        break
+    print despesa, valor
